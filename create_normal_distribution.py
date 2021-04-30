d386b33dd18a3cccb5ad09ad727e18351481c8e6
xiaoAwb <1031948738@bupt.edu.cn>
2021-04-30 17:16:58 +0800

    Update create_normal_distribution.py

--- create_normal_distribution.py
+++ create_normal_distribution.py
@@ -2,9 +2,9 @@ import numpy as np
 from numpy.linalg import cholesky
 import matplotlib.pyplot as plt
 
-sampleNo = a;
-mu = b
-sigma = c
+sampleNo = size;
+mu = Mean
+sigma = Standard deviation
 np.random.seed(0)
 s = np.random.normal(mu, sigma, sampleNo )
 plt.hist(s, 30, normed=True)
